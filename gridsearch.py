7f4d8a2eeaeff28a71f0774d9bb9a8ef728de445
nikoczernin <nikoczernin@gmail.com>
2023-12-18 20:11:16 +0100

    grid search for wine done

--- gridsearch.py
+++ gridsearch.py
@@ -1,16 +1,17 @@
+import pickle
 import warnings
 from datetime import datetime
-from pprint import pprint
 
 import numpy as np
 import pandas as pd
+from sklearn.neural_network import MLPClassifier
 
 import parameter_range
 from HELP import get_multidimensional_combinations
 from test_model import test_model
 
 
-def get_parameters(input_data, output_data):
+def get_parameters(input_data, output_data, n_neurons_steps=5):
     # get the parameter ranges according to the input and output data
     ranges = parameter_range.get_MLP_parameter_ranges(input_data, output_data)
     # convert ranges to numpy arrays of actual values for grid search
@@ -18,7 +19,8 @@ def get_parameters(input_data, output_data):
                                                ranges["learning_rate_init"][1],
                                                4)
     ranges["alpha"] = np.logspace(ranges["alpha"][0], ranges["alpha"][1], 5)
-    ranges["hidden_layer_sizes"] = parameter_range.get_architecture_suggestions(input_data, output_data)
+    ranges["hidden_layer_sizes"] = parameter_range.get_architecture_suggestions(input_data, output_data,
+                                                                                n_neurons_steps=n_neurons_steps)
     # names of parameters
     parameter_names = list(ranges.keys())
     # possible values of parameters (list of lists)
@@ -33,7 +35,8 @@ def get_parameters(input_data, output_data):
 
 
 def grid_search(model, input_data, output_data, data_title=None, model_name="",
-                break_after=None, print_progress=True):
+                break_after=None, print_progress=True, skip_til=None,
+                n_neurons_steps=5):
     # keep the following scores:
     # fit_time
     # test_balanced_accuracy
@@ -43,16 +46,19 @@ def grid_search(model, input_data, output_data, data_title=None, model_name="",
     result_columns = ["fit_time", "test_balanced_accuracy", "test_f1_weighted",
                       "test_precision_weighted", "test_recall_weighted"]
 
-    param_names, _, param_combinations = get_parameters(input_data, output_data)
-
+    param_names, _, param_combinations = get_parameters(input_data, output_data,
+                                                        n_neurons_steps=n_neurons_steps)
     # pd.DataFrame with the results
     print(param_names + result_columns)
     all_results = pd.DataFrame(columns=param_names + result_columns)
 
-    print()
     n = len(param_combinations)
     for i, param_combo_list in enumerate(param_combinations):
-        if print_progress: print(f"{i}/{n}", param_combo_list, end="\r")
+        if skip_til is not None:
+            if i < skip_til:
+                continue
+
+        if print_progress: print(f"\r{i}/{n}", param_combo_list, end="")
 
         # param_combo_list is a list of args
         params_dict = {param_name: param_combo_list[i] for i, param_name in enumerate(param_names)}
@@ -60,6 +66,9 @@ def grid_search(model, input_data, output_data, data_title=None, model_name="",
         # create a model using these parameters
         warnings.filterwarnings("ignore") # supress warnings
         model_test_results = test_model(model, params_dict, input_data, output_data)
+        # if no results, continue
+        if model_test_results is None:
+            continue
         model_test_results = list(model_test_results[result_columns])
         # all info on this iteration
         info_full = param_combo_list + model_test_results
@@ -77,3 +86,23 @@ def grid_search(model, input_data, output_data, data_title=None, model_name="",
     model_name = "MLP"
     all_results.to_csv(f"data/results/grid_search_results_{data_title}_{model_name}_{current_time}.csv")
 
+def main():
+    with open("data/fertility/wine_x.pickle", "rb") as f:
+        input_data = pickle.load(f)
+    with open("data/fertility/wine_y.pickle", "rb") as f:
+        output_data = pickle.load(f)
+
+    data_title = "wine"
+    model = MLPClassifier
+    print("starting grid search")
+    grid_search(model=model,
+                input_data=input_data,
+                output_data=output_data,
+                data_title=data_title,
+                model_name="MLP",
+                n_neurons_steps=3,
+                #break_after=10
+                )
+
+if __name__ == '__main__':
+    main()
--- test_model.py
+++ test_model.py
@@ -1,6 +1,9 @@
+import pickle
+from pprint import pprint
+
 from sklearn.model_selection import cross_validate
 import pandas as pd
-
+from sklearn.neural_network import MLPClassifier
 
 
 def test_model(Model, params, X, y, k=5, mean=True, scoring=None):
@@ -23,9 +26,43 @@ def test_model(Model, params, X, y, k=5, mean=True, scoring=None):
             'recall_weighted': 'recall_weighted'}
 
     model = Model(**params)
-    cv_results = cross_validate(model, X, y, cv=k, scoring=scoring, return_train_score=True)
+    try:
+        cv_results = cross_validate(model, X, y, cv=k, scoring=scoring, return_train_score=True)
+    except ValueError as e:
+        print()
+        print("This model failed:")
+        pprint(params)
+        pprint(e)
+        return None
     # to pandas df
     cv_results = pd.DataFrame(cv_results)
     if mean:
         cv_results = cv_results.mean()
     return cv_results
+
+
+def main():
+    with open("data/biomed/biomed_x.pickle", "rb") as f:
+        x = pickle.load(f)
+    with open("data/biomed/biomed_y.pickle", "rb") as f:
+        y = pickle.load(f)
+
+    print(biomed_x.shape)
+    print(biomed_y.shape)
+
+    # get the parameters
+    # test the model
+    res = test_model(MLPClassifier,
+               {'activation': 'logistic',
+                'alpha': 1.0002302850208247,
+                'hidden_layer_sizes': (4,),
+                'learning_rate': 'constant',
+                'learning_rate_init': 1.0023052380778996,
+                'solver': 'lbfgs'
+                },
+               x, y)
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
